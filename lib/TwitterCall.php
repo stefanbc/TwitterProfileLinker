4104547a21a5d45a278974270cf8a6150e024c56
stefanbc <stefcosma@gmail.com>
2014-03-19 22:07:23 +0000

    Twitter API call is working

--- lib/TwitterCall.php
+++ lib/TwitterCall.php
@@ -1,4 +1,7 @@
 <?php
+    
+    header('Content-Type: application/json');
+    
     require_once('TwitterAPI.php');
     
     // Set access tokens here - see: https://dev.twitter.com/apps/
@@ -9,10 +12,13 @@
         'consumer_secret' => ""
     );
     
+    // Get the handle 
+    $handle = $_REQUEST['handle'];
+    
     // Perform a GET request and echo the response
     // Note: Set the GET field BEFORE calling buildOauth();
-    $url = 'https://api.twitter.com/1/users/lookup.json';
-    $getfield = '?screen_name=';
+    $url = 'https://api.twitter.com/1.1/users/lookup.json';
+    $getfield = '?screen_name=' . $handle;
     $requestMethod = 'GET';
     
     $twitter = new TwitterAPIExchange($settings);
