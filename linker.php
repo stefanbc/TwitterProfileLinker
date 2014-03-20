74c29f951559ac3e5a1b55bc4dbae3d7bc26d450
stefanbc <stefcosma@gmail.com>
2014-03-20 19:41:11 +0000

    Started settings page

--- linker.php
+++ linker.php
@@ -10,20 +10,46 @@
         License URI: http://www.gnu.org/licenses/gpl-2.0.html
     */
     
-    // Add option in General Settings for Twitter hovecards
-    $new_general_setting = new new_general_setting();
-    // Create the checkbox in General settings
-    class new_general_setting {
-        function new_general_setting( ) {
-            add_filter( 'admin_init' , array( &$this , 'register_fields' ) );
-        }
-        function register_fields() {
-            register_setting( 'general', 'twitterHovercards', 'esc_attr' );
-            add_settings_field('twitterHovercards', '<label for="twitterHovercards">'.__('Enable Twitter Hovercards?' , 'twitterHovercards' ).'</label>' , array(&$this, 'fields_html') , 'general' );
-        }
-        function fields_html() {
-            echo '<input type="checkbox" id="twitterHovercards" name="twitterHovercards" value="1" ' . checked(1, get_option('twitterHovercards'), false) . '/>';
-        }
+    // Create submenu page in the WordPress Settings menu
+    function profile_linker() {
+        add_submenu_page('options-general.php', 'Twitter Profile Linker', 'Twitter Profile Linker', 'edit_posts', 'profile_linker', 'profile_linker_options');
+    }
+    add_action('admin_menu','profile_linker');
+    
+    // Add settings link to plugin on plugins list
+    function profile_linker_add_settings_link($links) {
+        $settings_link = '<a href="options-general.php?page=profile_linker">Settings</a>';
+        array_push($links, $settings_link);
+        return $links;
+    }
+    $plugin = plugin_basename(__FILE__);
+    add_filter("plugin_action_links_$plugin", 'profile_linker_add_settings_link');
+    
+    // Settings page
+    function profile_linker_options() {
+        
+        // Add the needed options to the wp_options table
+        add_option('twitterHovercards', $twitterHovercards);
+        add_option('twitterHovercards_oAuthAccessToken', $twitterHovercards_oAuthAccessToken);
+        add_option('twitterHovercards_oAuthAccessTokenSecret', $twitterHovercards_oAuthAccessTokenSecret);
+        add_option('twitterHovercards_consumerKey', $twitterHovercards_consumerKey);
+        add_option('twitterHovercards_consumerKeySecret', $twitterHovercards_consumerKeySecret);
+        
+        echo '<label for="twitterHovercards">'.__('Enable Twitter Hovercards?' , 'twitterHovercards' ).'</label>';
+        echo '<input type="checkbox" id="twitterHovercards" name="twitterHovercards" value="1" ' . checked(1, get_option('twitterHovercards'), false) . '/>';
+    
+        echo '<label for="twitterHovercards_oAuthAccessToken">'.__('Twitter oAuth Access Token' , 'twitterHovercards' ).'</label>';
+        echo '<input type="text" id="twitterHovercards_oAuthAccessToken" name="twitterHovercards_oAuthAccessToken" value="' . get_option('twitterHovercards_oAuthAccessToken') . '"/>';
+        
+        echo '<label for="twitterHovercards_oAuthAccessTokenSecret">'.__('Twitter oAuth Access Token Secret' , 'twitterHovercards' ).'</label>';
+        echo '<input type="text" id="twitterHovercards_oAuthAccessTokenSecret" name="twitterHovercards_oAuthAccessTokenSecret" value="' . get_option('twitterHovercards_oAuthAccessTokenSecret') . '"/>';
+        
+        echo '<label for="twitterHovercards_consumerKey">'.__('Twitter Consumer Key' , 'twitterHovercards' ).'</label>';
+        echo '<input type="text" id="twitterHovercards_consumerKey" name="twitterHovercards_consumerKey" value="' . get_option('twitterHovercards_consumerKey') . '"/>';
+        
+        echo '<label for="twitterHovercards_consumerKeySecret">'.__('Twitter Consumer Key Secret' , 'twitterHovercards' ).'</label>';
+        echo '<input type="text" id="twitterHovercards_consumerKeySecret" name="twitterHovercards_consumerKeySecret" value="' . get_option('twitterHovercards_consumerKeySecret') . '"/>';
+        
     }
     
     // Register styles and scripts to front for the Twitter Hovercards
@@ -33,13 +59,11 @@
         // If the option is ticked then we enqueue the style and script
         if($hovercards){
             // Register the styles
-            wp_register_style('twitterHovercards_font', 'https://fonts.googleapis.com/css?family=Roboto:300,400');
             wp_register_style('twitterHovercards_css', plugins_url('hovercards.css', __FILE__ ));
             // Register the script
             wp_register_script('twitterHovercards_script', plugins_url('hovercards.js' , __FILE__ ), array('jquery'));
             
             // Enqueue styles
-            wp_enqueue_style('twitterHovercards_font');
             wp_enqueue_style('twitterHovercards_css');
             // Enqueue the script
             wp_enqueue_script('twitterHovercards_script');
